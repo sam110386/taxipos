28a1c537cdb2acb0ee7d30e816bf1f2f70fc88b3
Akash kumar <akashbhopewal@gmail.com>
2022-05-03 13:16:13 +0530

    CallerIdInfo Table Complete

--- src/blocks/Triplog/CallerIdInfo.js
+++ src/blocks/Triplog/CallerIdInfo.js
@@ -4,9 +4,10 @@ import CallerIdDetails from "./CallerIdDetails";
 
 const CallerIdInfo = () => {
 
-    let arr1 = [1,,2,3,4]
+
     const [callerTripInfo, setCallerTripInfo] = useState([])
     const [showDetails, setShowDetails] = useState(false);
+    let ar = [1,2,3,4]
  
 
     const onError = () => {
@@ -49,141 +50,47 @@ const CallerIdInfo = () => {
         setShowDetails(true)
     }
 
-   
-    const arrr = [
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "1",
-                "Phone": "10",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "2",
-                "Phone": "20",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "3",
-                "Phone": "30",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "4",
-                "Phone": "40",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "5",
-                "Phone": "50",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-        {
-            UrlSend: {
-                "id": "59",
-                "Line": "6",
-                "Phone": "60",
-                "Name": "Imran",
-                "created": "2022-05-02 04:40:34"
-            }
-        },
-
-
-     
-
-    ]
-
 
     return (
         <>
             <div id="latestCallerInfo" >
-            {/* <table class="blank_ul">
-
-                <tr class="inner_div">
-                <th class="mid_th comtab" >F1</th>
-                <th class="mid_th text-center" width="100"  ></th>
-                <th class="mid_th"></th>
-                </tr>
-                <tr class="inner_div">
-                <th class="mid_th comtab" >F2</th>
-                <th class="mid_th text-center" width="100"  ></th>
-                <th class="mid_th"></th>
-                </tr>
-                <tr class="inner_div">
-                <th class="mid_th comtab" >F3</th>
-                <th class="mid_th text-center" width="100"  ></th>
-                 <th class="mid_th"></th>
-                </tr>
-
-            </table> */}
-            
-                {arr1.length > 0 && arr1.map((el, index, caller) => {
-                    
+                {ar.length > 0 && ar.map((el, index, caller) => {
+                    if (index < 4) {
                         return (
                             <table class="blank_ul">
-
-                                {callerTripInfo.map((el2, index2,arr) => {
-                                    
+                                {ar.map((el2, index2,arr) => {
                                     if (index2 < 3) {
                                         let f_numb;
                                         if (index == 0) {
                                             f_numb = index2 + 1;
                                         } else if (index == 1) {
-                                            f_numb = index2 + 3;
+                                            f_numb = index2 + 4;
                                         } else if (index == 2) {
-                                            f_numb = index2 + 6;
+                                            f_numb = index2 + 7;
                                         } else {
-                                            f_numb = index2 + 9;
+                                            f_numb = index2 + 10;
                                         }
-
-                                        let j = 0;
-                                        let th_flag = 0;
-
                                         return (
                                             <tr class="inner_div">
-                                                <th class="mid_th comtab" >F{f_numb}</th>
-                                                {arr.map((ele3,index3,array)=>{
+                                               <th class="mid_th comtab" >F{f_numb}</th>
+                                                {callerTripInfo.map((ele3)=>{
                                                     if(ele3.UrlSend.Line == f_numb){
-                                                        th_flag = 1
                                                         return (
-                                                            <>
+                                                            <>  
                                                             <th class="mid_th text-center" width="100"  >{ele3.UrlSend.Phone}</th>
-                                                            <th class="mid_th"></th>
+                                                            <th class="mid_th">...</th>
                                                             </>
-                                                        )
-                                                        
+                                                        ) 
                                                     }
-                                                    j++
                                                 })}
-                                               
                                             </tr>
                                         )
-                                        
                                     }
                                 }
                                 )}
-
                             </table>
                         )
-                    
+                    }
                 })}
             </div>
 
