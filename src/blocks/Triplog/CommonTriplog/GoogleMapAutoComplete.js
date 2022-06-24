66c84fc1556ccc3f5c7eab42c4e30d923022ebe3
vinod <Robinsaini2126@gmail.com>
2022-06-24 18:31:43 +0530

    user api keys

--- src/blocks/Triplog/CommonTriplog/GoogleMapAutoComplete.js
+++ src/blocks/Triplog/CommonTriplog/GoogleMapAutoComplete.js
@@ -3,8 +3,8 @@ import { store } from "../../../store/store";
 
 export const loader = new Loader(
   `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=${
-    store.getState().profile.pubnubkeys === null
+    store.getState().profile.googleplaceapikey === null
       ? process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-      : store.getState().profile.pubnubkeys
+      : store.getState().profile.googleplaceapikey
   }&libraries=places`
 );
