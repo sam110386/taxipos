d43e3d22570e9db9dc59e88ded8b3e46dd1d0907
vinod <Robinsaini2126@gmail.com>
2022-07-07 10:43:56 +0530

    car number

--- src/blocks/Pickers/CarValue.js
+++ src/blocks/Pickers/CarValue.js
@@ -1,15 +1,28 @@
 import React, { useEffect, useState } from "react";
-const CarValue = ({carName,rel_affiliate_dispache,rel_parent_dispacher,rel_current_dispacher}) => {
+const CarValue = ({
+  carName,
+  rel_affiliate_dispache,
+  rel_parent_dispacher,
+  rel_current_dispacher,
+}) => {
   const [car_Name, setCarName] = useState(carName);
   const [toogle, setToogle] = useState(false);
   const handleChnage = (event) => {
     setCarName(event.target.value);
   };
+// true && true || false
   return (
     <>
-    {toogle?   <input value={car_Name} onChange={handleChnage} />:
-    <span onClick={() => setToogle(true)}>{car_Name}</span>
-    }
+      {rel_current_dispacher === rel_parent_dispacher ||
+      rel_current_dispacher === rel_affiliate_dispache ? (
+        toogle ? (
+          <input value={car_Name} onChange={handleChnage} />
+        ) : (
+          <span onClick={() => setToogle(true)}>{car_Name}</span>
+        )
+      ) : (
+        <span>{car_Name}</span>
+      )}
     </>
   );
 };
